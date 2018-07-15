549d75b5a30ce1df11478a59e3fee72e4659348a
Tony Thomas <thoma127@umn.edu>
2018-07-15 13:40:23 -0500

    Yarn setup and new test. No module yet.

---
+++ test/new_change_request_submit_button.test.js
@@ -0,0 +1,9 @@
+const jquery = require("jquery");
+const NewChangeRequestSubmitButton = require ("../src/new_change_request_submit_button");
+
+describe("NewChangeRequestSubmitButton", () => {
+  it("should toggle disable based on whether the reason field is empty", () => {
+    // Instantiate our object using the same selector
+    const submit_button = $("#new_change_request input[name=commit]");
+  });
+});
